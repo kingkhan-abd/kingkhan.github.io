7144531024cc1280d332aef96d23dc9da934d1b9
kingkhan-abd <eijazking6791@gmail.com>
2023-02-07 00:28:35 +0500

    this is jsclass5 assignmetn

---
+++ app.js
@@ -0,0 +1,88 @@
+// var lang = prompt("Enter your language");
+
+// if(lang == "HTML"){
+//     alert("you are  hired!")
+// }
+// else{
+//     alert("you are not hired!");
+// }
+
+
+
+
+// var food = prompt("Enter your food item!");
+
+// if(food === "biryani"){
+//     var extraitem = prompt("Enter your extra item!");
+//     if(extraitem === "raita"){
+
+//         alert("This is biryani dish");
+//     }
+//     else{
+//         alert("chordo ajao!" + food + " laka");
+//     }
+
+// }
+// else if(food === "burger"){
+//     alert("This is burger ");
+// }
+// else if(food === "fries"){
+//     alert("This is fries");
+// }
+// else{
+//     alert("ghar mat aao");
+// }
+// var lang1 = prompt("Enter your lang1");
+
+
+// if(lang1 === "HTML"){  
+//     var lang2 = prompt("Enter your lang2");
+//     if(lang2 === "CSS"){
+//         alert("your are hired!");
+
+//     }
+
+//     else{
+//         alert("Apply after learning CSS!");
+//     }
+// }
+// else{
+//     alert("your are not hired!");
+// }
+
+
+
+var ghous;
+var Ahmed;
+var abc = true && "ghous";
+
+console.log(abc);
+ ghous
+
+var abc = false || "ghous";
+
+console.log(abc);
+ ghous
+
+var abc = false && "ghous";
+
+console.log(abc);
+ false
+
+var abc = true && "ghous";
+
+console.log(abc);
+ ghous
+
+var abc = true && false || "Ahmed";
+
+
+console.log(abc);
+ Ahmed
+
+var abc = true && true || "Ahmed";
+
+
+
+console.log(abc);
+ true
